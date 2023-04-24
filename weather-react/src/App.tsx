3cac0140fbf47f85539016f8f6cd6fe60f51b570
Vladimir Orlov <satancrew.true@gmail.com>
2023-04-24 16:51:59 +0300

    feat: add router

--- weather-react/src/App.tsx
+++ weather-react/src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {
+  Route,
+  Routes,
+} from 'react-router-dom';
+import { Home } from './pages/Home/Home';
+import { Statistic } from './pages/Statistic/Statistic';
 import './styles/index.scss';
 
 function App() {
   return (
-    <div className="App">Привет</div>
+    <Routes>
+      <Route
+        path="/"
+        element={<Home />}
+      />
+      <Route
+        path="/statistic"
+        element={<Statistic />}
+      />
+    </Routes>
   );
 }
 
