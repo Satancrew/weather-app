3f608c589df934e72e68aad416aedd89e59e9dfb
Vladimir Orlov <satancrew.true@gmail.com>
2023-04-25 13:55:53 +0300

    feat: finish header

--- weather-react/src/App.tsx
+++ weather-react/src/App.tsx
@@ -5,20 +5,24 @@ import {
 } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { Statistic } from './pages/Statistic/Statistic';
+import { Header } from './shared/Header/Header';
 import './styles/index.scss';
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Home />}
-      />
-      <Route
-        path="/statistic"
-        element={<Statistic />}
-      />
-    </Routes>
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/statistic"
+          element={<Statistic />}
+        />
+      </Routes>
+    </div>
   );
 }
 
