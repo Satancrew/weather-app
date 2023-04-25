493d0112780c0900bff750907799fec6df729c0d
Vladimir Orlov <satancrew.true@gmail.com>
2023-04-25 17:02:29 +0300

    feat: add component thisDayInfo

--- weather-react/src/pages/Home/Home.tsx
+++ weather-react/src/pages/Home/Home.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from './Home.module.scss';
 import { ThisDay } from './components/ThisDay/ThisDay';
+import { ThisDayInfo } from './components/ThisDayInfo/ThisDayInfo';
 
 type Props = {};
 
 export const Home = (props: Props) => {
   return (
     <div className={styles.home}>
-      <ThisDay />
+      <div className={styles.wrapper}>
+        <ThisDay />
+        <ThisDayInfo />
+      </div>
     </div>
   );
 };
